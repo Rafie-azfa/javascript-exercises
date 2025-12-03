f4d366ed41ac996cb95458a07e606c5167dc87d5
fie <rafieazfa.z@gmail.com>
2025-12-03 16:00:15 +1100

    Complete: removeFromArray() Implementation

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr) {
+    const args = Array.prototype.slice.call(arguments, 1);
+    return arr.filter( (item) => {
+        for(arg of args){
+            return item !== arg;
+        }
+    })
 };
 
 // Do not edit below this line
