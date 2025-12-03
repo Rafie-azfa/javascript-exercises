fb844977b14c25775456d08c5a7384de0d0a34db
fie <rafieazfa.z@gmail.com>
2025-12-04 10:09:47 +1100

    Complete: convertToCelsius() and convertToFahrenheit() Implementation

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let formulaFahrenheit = (temp - 32) * (5/9);
+  return Math.round(formulaFahrenheit * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let formulaCelsius = temp * (9/5) + 32;
+  return Math.round(formulaCelsius * 10) / 10;
 };
 
 // Do not edit below this line
