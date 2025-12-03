aba833d0112e726d7602dd6988680b56076c431d
fie <rafieazfa.z@gmail.com>
2025-12-04 09:37:43 +1100

    Complete: sumAll() Implementation

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(numA, numB) 
+{
+     if(numA < 0 || numB < 0 || !Number.isInteger(numA) || !Number.isInteger(numB)){
+        return "ERROR";
+    }  
+    
+    const minNum = Math.min(numA, numB);
+    const maxNum = Math.max(numA, numB);
 
+    let result = 0;
+    for(let i = minNum; i <= maxNum; i++){
+        result += i;
+    }
+    return result; 
 };
 
 // Do not edit below this line
